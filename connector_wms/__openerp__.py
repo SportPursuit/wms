f29f5fab6ccc4666d244409e099f9baf445c0ac3
DHS <davehs@gmail.com>
2016-05-17 16:42:49 +0100

    Fix version numbers

--- connector_wms/__openerp__.py
+++ connector_wms/__openerp__.py
@@ -19,8 +19,8 @@
 ##############################################################################
 
 {'name': 'Connector for WMS',
-' 'version': 7.20160516.0,
-' 'category': 'Connector',
+ 'version': '7.20160516.0',
+ 'category': 'Connector',
  'author': 'credativ Ltd',
  'website': 'http://www.credativ.co.uk',
  'license': 'AGPL-3',
