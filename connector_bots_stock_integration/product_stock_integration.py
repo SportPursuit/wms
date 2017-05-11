58282cb3bbe875adc56ea7d4cdcaded9378dca68
Dermot O'Sullivan <dermot.osullivan@sportpursuit.co.uk>
2017-05-11 14:11:33 +0100

    [#721] Read from the new stock location for supplier_virtual_available_combined when its enabled for a supplier

--- connector_bots_stock_integration/product_stock_integration.py
+++ connector_bots_stock_integration/product_stock_integration.py
@@ -1,22 +1,47 @@
 from openerp.osv import osv, fields
 import openerp.addons.decimal_precision as dp
 
-    
-class product(osv.osv):
+from .supplier_stock import SUPPLIER_STOCK_FEED
+
+
+class product_product(osv.osv):
     _inherit = "product.product"
     
     def _product_available_supplier(self, cr, uid, ids, field_names=None, arg=False, context=None):
-        res = super(product,self)._product_available_supplier(cr, uid, ids, field_names=field_names, arg=arg, context=context)
-        integration_qty_data = self.read(cr, uid, ids, ['supplier_stock_integration_qty'])
-        integration_qty = {v['id']: v['supplier_stock_integration_qty'] for v in integration_qty_data}
-        for id in ids:
-            res[id]['supplier_virtual_available_combined'] = res[id]['supplier_virtual_available_combined'] + integration_qty.get(id, 0.0)
-        return res
-    
+
+        field_names = field_names or []
+
+        if 'supplier_virtual_available_combined' not in field_names:
+            return super(product_product, self)._product_available_supplier(
+                cr, uid, ids, field_names=field_names, arg=arg, context=context
+            )
+
+        feed_enabled_products = []
+        feed_disabled_products = []
+
+        for product in self.browse(cr, uid, ids, context=context):
+            if any(product.seller_ids.name.stock_feed_enabled):
+                feed_enabled_products.append(product.id)
+            else:
+                feed_disabled_products.append(product.id)
+
+        result = {}
+
+        if feed_enabled_products:
+            c = context.copy()
+            c['location'] = SUPPLIER_STOCK_FEED
+            products = self.get_product_available(cr, uid, feed_enabled_products, context=c)
+            for product, qty in products.iteritems():
+                result[product]['supplier_virtual_available_combined'] = qty
+
+        else:
+             result.update( super(product_product,self)._product_available_supplier(
+                cr, uid, ids, field_names=field_names, arg=arg, context=context
+            ))
+
+        return result
+
     _columns = {
-        'supplier_stock_integration_qty': fields.float(
-            string='Supplier Integration Quantity', digits_compute=dp.get_precision('Product Unit of Measure')
-        ),
         'supplier_virtual_available_combined': fields.function(
             _product_available_supplier, multi='supplier_virtual_available',
             type='float',  digits_compute=dp.get_precision('Product Unit of Measure'),
@@ -24,6 +49,6 @@ class product(osv.osv):
             help="Forecast quantity (computed as Quantity On Hand "
                  "- Outgoing + Incoming) at the virtual supplier location "
                  "for the current warehouse if applicable, otherwise 0, plus "
-                 "the normal forecast quantity and quantity provided by the supplier feed."
+                 "the normal forecast quantity OR quantity provided by the supplier stock feed."
         ),
     }
--- connector_bots_stock_integration/supplier_stock_integration.py
+++ connector_bots_stock_integration/supplier_stock_integration.py
@@ -4,6 +4,10 @@ class supplier(osv.osv):
     _inherit = 'res.partner'
     
     _columns = {
+        'stock_feed_enabled': fields.boolean(
+            'Enable Stock Feed Integration',
+            help="Use supplier quantity supplied via stock feed instead of normal backorder limit"
+        ),
         'percent_to_exclude': fields.integer(
             'Stock % to Exclude',
             help="Percentage of stock to be excluded from the supplier available quantity when importing stock quantities through the supplier feed"
