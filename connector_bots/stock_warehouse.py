0b2aca1c9bbb72e82e5361d3d6880b9d4f35a8d9
Dermot O'Sullivan <dermot.osullivan@sportpursuit.co.uk>
2017-05-24 17:07:09 +0100

    [#OppEff-289] Fix error string

--- connector_bots/stock_warehouse.py
+++ connector_bots/stock_warehouse.py
@@ -446,7 +446,7 @@ class WarehouseAdapter(BotsCRUDAdapter):
                             main_picking = bots_picking_obj.browse(_cr, self.session.uid, main_picking_id, context=ctx)
 
                             if picking['type'] == 'in' and main_picking.openerp_id.shipped is True:
-                                raise JobError('PO %s is already received')
+                                raise JobError('PO %s is already received' % main_picking.openerp_id.name)
 
                             picking_ids = [main_picking.openerp_id.id]
                             ctx.update({'company_id' : main_picking.openerp_id.company_id.id})
