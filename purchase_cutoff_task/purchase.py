402b9a7657b030e5d1235f0daa7220e355e81ed0
Michele Sarigu <michele.sarigu@sportpursuit.co.uk>
2019-04-04 15:02:38 +0100

    fixing purchase_cutoff for multiple warehouses

--- purchase_cutoff_task/purchase.py
+++ purchase_cutoff_task/purchase.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
 # (c) 2016 credativ ltd. - Ondřej Kuzník
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+import logging
 
 from openerp.osv import orm
 
 from openerp.addons.queue_tasks.queue_task import defer
+
+logger = logging.getLogger(__name__)
+
 
 class PurchaseOrder(orm.Model):
     _inherit = 'purchase.order'
 
     @defer("Cut-off Purchase Order")
     def purchase_cutoff_defer(self, cr, uid, ids, context=None):
-        warehouse_pool = self.pool.get('bots.warehouse')
+        bots_warehouse = self.pool.get('bots.warehouse')
         for group in self.read_group(cr, uid, [('id', 'in', ids)], ['warehouse_id'], ['warehouse_id'], context=context):
-            warehouse = warehouse_pool.browse(cr, uid, group['warehouse_id'][0], context=context)
+            logger.info("Groups: %s" % group)
+            warehouse = bots_warehouse.browse(cr, uid, bots_warehouse.search(
+                cr, uid, [('warehouse_id', '=', group['warehouse_id'][0])]
+            ), context=context)
             purchase_ids = self.search(cr, uid, group['__domain'], context=context)
-            warehouse.purchase_cutoff(purchase_ids)
+            warehouse[0].purchase_cutoff(purchase_ids)
