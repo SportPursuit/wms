39c0cef1582b5d42588676143f7fd45b1607dfcd
Michele Sarigu <michele.sarigu@sportpursuit.co.uk>
2019-06-03 12:19:33 +0100

    fixing accessing res.partner

--- connector_bots_stock_integration/product_stock_integration.py
+++ connector_bots_stock_integration/product_stock_integration.py
@@ -45,7 +45,7 @@ class product_product(osv.osv):
 
     def _product_available_supplier_feed(self, cr, uid, ids, field_names=None, arg=False, context=None):
         supplier = context.get('supplier', False)
-        partner_obj = self.pool.get('res_partner')
+        partner_obj = self.pool.get('res.partner')
 
         supplier = partner_obj.browse(cr, uid, supplier, context=context)
         return self._get_available_supplier_feed_qty(cr, uid, ids, supplier.default_warehouse_id)
