846f8040b487a0291208aaa56cc23bd7f5c0aeed
scrampin <stephanie.crampin@sportpursuit.co.uk>
2020-08-28 15:40:49 +0100

    Update ctx with sp_invoice_no_store

--- connector_bots/stock_warehouse.py
+++ connector_bots/stock_warehouse.py
@@ -648,6 +648,8 @@ class WarehouseAdapter(BotsCRUDAdapter):
 
         ctx = self.session.context.copy()
         ctx['wms_bots'] = True
+        if moves_to_process:
+            ctx['sp_invoice_no_store'] = True
 
         _cr = self.session.cr
 
