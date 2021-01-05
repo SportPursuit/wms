2af2ac5c5ce79b730145c19827fca279c01b6603
scrampin <stephanie.crampin@sportpursuit.co.uk>
2021-01-05 18:10:36 +0000

    delay picking export bydefined amount

--- connector_bots/stock.py
+++ connector_bots/stock.py
@@ -1198,27 +1198,9 @@ def delay_export_picking_out(session, model_name, record_id, vals):
         else:
             raise Exception('Unable to create a unique bots id')
 
-    delivery_country = picking.openerp_id.partner_id.country_id.code
-    picking_warehouse_code = picking.openerp_id.warehouse_id.code
-    if INTERNATIONAL_WAREHOUSE_MAPPING.get(delivery_country, '') == picking_warehouse_code:
-        cr = session.cr
-        uid = session.uid
-        picking_obj = session.pool.get('stock.picking')
-        bots_obj = session.pool.get(model_name)
-        order_id = picking.openerp_id.sale_id.id
-        split_picking_delay = EXPORT_PICKING_DELAY_DAYS * 86400
-        delivery_order_ids = picking_obj.search(cr, uid, [('sale_id', '=', order_id)])
-        delivery_orders = [picking_obj.browse(id) for id in delivery_order_ids if id != picking.openerp_id.id]
-        for do in delivery_orders:
-            if do.warehouse_id.code == INTERNATIONAL_WAREHOUSE_MAPPING.get(delivery_country):
-                do_bots_records = bots_obj.search(cr, uid, [('openerp_id', '=', do.id)])
-                if do_bots_records:
-                    bots_record = bots_obj.browse(cr, uid, do_bots_records[0])
-                    if bots_record.bots_id:
-                        update_date = bots_record.write_date
-                        if datetime.now() < update_date + timedelta(EXPORT_PICKING_DELAY_DAYS):
-                            delay += split_picking_delay
-                            break
+    if picking.openerp_id.bots_export_delay:
+        export_delay = picking.openerp_id.bots_export_delay * 86400
+        delay += export_delay
 
     export_picking.delay(session, model_name, record_id, eta=delay, priority=EXPORT_PICKING_PRIORITY)
 
