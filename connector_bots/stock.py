c44c80161b401c003d1bbc0a351692cde16b0862
scrampin <stephanie.crampin@sportpursuit.co.uk>
2021-01-09 15:46:52 +0000

    break out delay to additional method

--- connector_bots/stock.py
+++ connector_bots/stock.py
@@ -1171,6 +1171,35 @@ class BotsPickingExport(ExportSynchronizer):
         pass
 
 
+def get_additional_delay(session, picking):
+    delivery_country = picking.openerp_id.partner_id.country_id.code
+    picking_warehouse_code = picking.openerp_id.warehouse_id.code
+    additional_delay = 0
+    if INTERNATIONAL_WAREHOUSE_MAPPING.get(delivery_country, '') == picking_warehouse_code:
+        cr = session.cr
+        uid = session.uid
+        picking_obj = session.pool.get('stock.picking')
+        warehouse_obj = session.pool.get('stock.warehouse')
+        bots_obj = session.pool.get(model_name)
+        order_id = picking.openerp_id.sale_id.id
+        wh_delay = warehouse_obj.browse(session.cr, session.uid, picking.warehouse_id.id).picking_export_delay
+        split_picking_delay_seconds = wh_delay * 3600
+        split_picking_delay_days = wh_delay / 24
+        delivery_order_ids = picking_obj.search(cr, uid, [('sale_id', '=', order_id)])
+        delivery_orders = [picking_obj.browse(id) for id in delivery_order_ids if id != picking.openerp_id.id]
+        for do in delivery_orders:
+            if do.warehouse_id.code == INTERNATIONAL_WAREHOUSE_MAPPING.get(delivery_country):
+                do_bots_records = bots_obj.search(cr, uid, [('openerp_id', '=', do.id)])
+                if do_bots_records:
+                    bots_record = bots_obj.browse(cr, uid, do_bots_records[0])
+                    if bots_record.bots_id and bots_record.sync_date:
+                        sync_date_plus_offset = bots_record.sync_date + timedelta(days=split_picking_delay_days)
+                        if datetime.now() < sync_date_plus_offset:
+                            additional_delay = split_picking_delay_seconds
+                            break
+        return additional_delay
+
+
 @on_record_create(model_names='bots.stock.picking.out')
 def delay_export_picking_out(session, model_name, record_id, vals):
     # By default delay for 15 mins (configurable) to allow multiple moves becoming available within a short time to be exported together
@@ -1199,30 +1228,8 @@ def delay_export_picking_out(session, model_name, record_id, vals):
         delay += export_delay
 
     else:
-        delivery_country = picking.openerp_id.partner_id.country_id.code
-        picking_warehouse_code = picking.openerp_id.warehouse_id.code
-        if INTERNATIONAL_WAREHOUSE_MAPPING.get(delivery_country, '') == picking_warehouse_code:
-            cr = session.cr
-            uid = session.uid
-            picking_obj = session.pool.get('stock.picking')
-            warehouse_obj = session.pool.get('stock.warehouse')
-            bots_obj = session.pool.get(model_name)
-            order_id = picking.openerp_id.sale_id.id
-            wh_delay = warehouse_obj.browse(session.cr, session.uid, picking.warehouse_id.id).picking_export_delay
-            split_picking_delay_seconds = wh_delay * 3600
-            split_picking_delay_days = wh_delay / 24
-            delivery_order_ids = picking_obj.search(cr, uid, [('sale_id', '=', order_id)])
-            delivery_orders = [picking_obj.browse(id) for id in delivery_order_ids if id != picking.openerp_id.id]
-            for do in delivery_orders:
-                if do.warehouse_id.code == INTERNATIONAL_WAREHOUSE_MAPPING.get(delivery_country):
-                    do_bots_records = bots_obj.search(cr, uid, [('openerp_id', '=', do.id)])
-                    if do_bots_records:
-                        bots_record = bots_obj.browse(cr, uid, do_bots_records[0])
-                        if bots_record.bots_id and bots_record.sync_date:
-                            sync_date = bots_record.sync_date
-                            if datetime.now() < sync_date + timedelta(days=split_picking_delay_days):
-                                delay += split_picking_delay_seconds
-                                break
+        additional_delay = get_additional_delay(session, picking)
+        delay += additional_delay
 
     export_picking.delay(session, model_name, record_id, eta=delay, priority=EXPORT_PICKING_PRIORITY)
 
